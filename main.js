27fe957d55e2c0beaee821cc6bb58cc353916306
Jayson <jayson@pop-os.localdomain>
2020-02-21 15:59:13 -0500

    Update

--- main.js
+++
@@ -1,4 +0,0 @@
-
-( window ).resize(function() {
-  $( "#log" ).append( "<div>Handler for .resize() called.</div>" );
-});
---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+/*window.onload = () => {
+
+    let buttons = document.querySelectorAll('button');
+
+
+    buttons.forEach((button) => {
+
+      console.log(button.clientWidth);
+      console.log(button.clientHeight);
+
+      button.style.height = `${ button.clientWidth }px`;
+
+    });
+
+}
