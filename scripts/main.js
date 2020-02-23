6f6baffbdde061fc743b13688013a607b91bd5ef
Karl.Kain <57959111+KarlKain@users.noreply.github.com>
2020-02-22 21:54:55 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,17 @@
+class Calculator {
+    constructer(currentOperandTextElement) {
+        this.currentOperandTextElement = currentOperandTextElement
+    }    
 /*window.onload = () => {
 
+ appendNumber(number) {
+     this.currentOperand = number
+ }
+
+ upadateDisplay() {
+     this.currentOperandTextElement.innerText = this.currnetOperand
+ }
+} 
     const buttons = document.querySelectorAll('button');
     const currentOperandTextElement = document.querySelector('
     [data-current-operand]')
@@ -8,7 +20,9 @@
 
       console.log(button.clientWidth);
       console.log(button.clientHeight);
-
+      button addEventListener('click', () => {
+        caculator.appendNumber(button.innerText)
+        caculator.updateDislplay()
       button.style.height = `${ button.clientWidth }px`;
 
     });
