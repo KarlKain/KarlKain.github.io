1d175d7d7fa4ac4e32fb2126440cab17d58f67b3
Karl.Kain <57959111+KarlKain@users.noreply.github.com>
2020-02-25 21:18:11 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,43 +1,51 @@
-
 class Calculator {
     constructor(previousOperandTextElement,currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement
         this.currentOperandTextElement = currnetOperandTextElement
         this.clear()
-    }    
+}    
 
- clear() {
-   this.currentOperand = ''
-   this.previousOperand = ''
-   this.operation = undefined
- }
- 
- delete() {
- 
- }
- appendNumber(number) {
-     this.currentOperand = number
- }
-
- chooseOperation(operation) {
- 
- }
- 
- compute() {
- 
- }
- 
-updateDisplay() {
- this.currentOperandTextElement.innerText = this.currentOperand
+clear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
 }
  
-    
+delete() {
+ 
+}
+appendNumber(number) {
+    this.currentOperand = number
+}
+
+chooseOperation(operation) {
+ 
+}
+ 
+compute() {
+ 
+}
+ 
+updateDisplay() {
+    this.currentOperandTextElement.innerText = this.currentOperand
+}
+ 
     const buttons = document.querySelectorAll('button');
-    const currentOperandTextElement = document.querySelector('
-    [data-current-operand]')
+   
+const previousOperandTextElement = document.querySelector('
+[data-previous-operand]')
+  
+const currentOperandTextElement = document.querySelector('
+[data-current-operand]')
 
-    buttons.forEach((button) => {
+const calculater = new Calculater(previousOperandTextElement,currentOperandTextElement)
 
+numberButtons.foreEach(button => {
+    button.addEventListener('click', () => {
+        calculater.appendnumber(button.innertext)
+        calculater.upadateDisplay()
+    })
+})                                                                 
       console.log(button.clientWidth);
       console.log(button.clientHeight);
       button addEventListener('click', () => {
