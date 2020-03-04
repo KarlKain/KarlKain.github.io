dcf4cf8ff13931805ea803afd066b262b624e09b
Karl.Kain <57959111+KarlKain@users.noreply.github.com>
2020-03-03 20:12:39 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,70 +1,70 @@
 class Calculator {
-    constructor(previousOperandTextElement,currentOperandTextElement) {
-        this.previousOperandTextElement = previousOperandTextElement
-        this.currentOperandTextElement = currnetOperandTextElement
-        this.clear()
-}    
+ constructor(previousOperandTextElement,currentOperandTextElement) {
+    this.previousOperandTextElement = previousOperandTextElement
+    this.currentOperandTextElement = currnetOperandTextElement
+    this.clear()
+  }    
 
-clear() {
-    this.currentOperand = ''
-    this.previousOperand = ''
-    this.operation = undefined
-}
+ clear() {
+     this.currentOperand = ''
+     this.previousOperand = ''
+     this.operation = undefined
+ }
  
-delete() {
-  this.currentOperand = this.currentOperamd.tostring().slice(0, -1)
-}
-appendNumber(number) {
-    if (number === '.' && this.currentOperand.includes('.')) return
-    this.currentOperand = this.currnetOperand.tostring() + number.tostring()
-}
+ delete() {
+   this.currentOperand = this.currentOperamd.tostring().slice(0, -1)
+ }
+ appendNumber(number) {
+     if (number === '.' && this.currentOperand.includes('.')) return
+     this.currentOperand = this.currnetOperand.tostring() + number.tostring()
+ }
 
-chooseOperation(operation) {
-    if (this.currentOperand === '') return
-    if (this.previousOperand !== '') {
-        this.compute()
-    }
-    this.operation = operation 
-    this.previousOperand = this.currentOperand
-    this.currentOperand = ''
+ chooseOperation(operation) {
+     if (this.currentOperand === '') return
+     if (this.previousOperand !== '') {
+         this.compute()
+     }
+     this.operation = operation 
+     this.previousOperand = this.currentOperand
+     this.currentOperand = ''
  
-}
+ }
 
-eval(expression){
- document.querySelectorAll('[data-number}').innerText 
-}    
-compute() {
-    let computation
-    const prev = prasefloat(this.previousOperand)
-    const current = paraseflpat(this.currentOperand)
-    if (isNaN(prev) || IsNaN(urrent)) return
-    switch (this.operation) {
-        case '+':
-          computation = prev + current
-           break
-        case '-':
-          computation = prev - current
-           break
-        case '*':
-          computation = prev * current
-           break
-        case '/':
-          computation = prev / current
-           break
-        case 'x^2':
-          computation = prev * prev
-           break
-    }
-    this.currentOperand = computation
-    this.operation = undefined
-    this.previousOperand = ''
-}
+ eval(expression){
+    document.querySelectorAll('[data-number}').innerText 
+ }    
+ compute() {
+     let computation
+     const prev = prasefloat(this.previousOperand)
+     const current = paraseflpat(this.currentOperand)
+     if (isNaN(prev) || IsNaN(urrent)) return
+     switch (this.operation) {
+         case '+':
+           computation = prev + current
+            break
+         case '-':
+           computation = prev - current
+            break
+         case '*':
+           computation = prev * current
+            break
+         case '/':
+           computation = prev / current
+            break
+         case 'x^2':
+           computation = prev * prev
+            break
+ }
+     this.currentOperand = computation
+     this.operation = undefined
+     this.previousOperand = ''
+ }
 
-updateDisplay(){
-    this.currentOperandTextElement.innerText = this.currentOperand  
-    this.previousOperandTextElement.innertext = this.previousOperand 
+ updateDisplay(){
+     this.currentOperandTextElement.innerText = this.currentOperand  
+     this.previousOperandTextElement.innertext = this.previousOperand 
+ }
 }
-
     
 const numberButtons =document.querySelectorAll('[data-number}')
 const operationButtons = document.querySelectorAll('[data-operation]')
